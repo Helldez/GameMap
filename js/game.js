7144a79bf955b888e5866ca51a6bbe373fbb8021
Helldez <74499579+Helldez@users.noreply.github.com>
2024-10-18 14:17:15 +0000

    Aggiornamento

--- js/game.js
+++ js/game.js
@@ -22,38 +22,38 @@ let player;
 let cursors;
 
 function preload() {
-    // Carichiamo la mappa predefinita e il tileset di Phaser
-    this.load.tilemapTiledJSON('map', 'https://labs.phaser.io/assets/tilemaps/maps/simple-map.json');
-    this.load.image('tiles', 'https://labs.phaser.io/assets/tilemaps/tiles/tileset.png');
+    // Carichiamo la mappa predefinita e il tileset
+    this.load.tilemapTiledJSON('map', 'https://examples.phaser.io/assets/tilemaps/maps/desert.json');
+    this.load.image('tiles', 'https://examples.phaser.io/assets/tilemaps/tiles/tmw_desert_spacing.png');
     
-    // Carichiamo un personaggio predefinito di Phaser
-    this.load.spritesheet('dude', 'https://labs.phaser.io/assets/sprites/dude.png', { frameWidth: 32, frameHeight: 48 });
+    // Carichiamo uno sprite predefinito per il personaggio
+    this.load.spritesheet('player', 'https://examples.phaser.io/assets/sprites/phaser-dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 function create() {
     // Creiamo la mappa
     const map = this.make.tilemap({ key: 'map' });
-    const tileset = map.addTilesetImage('tileset', 'tiles');
-    const layer = map.createLayer('Tile Layer 1', tileset, 0, 0);
+    const tileset = map.addTilesetImage('Desert', 'tiles');
+    const layer = map.createLayer('Ground', tileset, 0, 0);
 
     // Aggiungiamo il personaggio
-    player = this.physics.add.sprite(100, 150, 'dude');
+    player = this.physics.add.sprite(100, 150, 'player');
 
     // Anima il personaggio
     this.anims.create({
         key: 'left',
-        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 0, end: 3 }),
         frameRate: 10,
         repeat: -1
     });
     this.anims.create({
         key: 'turn',
-        frames: [{ key: 'dude', frame: 4 }],
+        frames: [{ key: 'player', frame: 4 }],
         frameRate: 20
     });
     this.anims.create({
         key: 'right',
-        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frames: this.anims.generateFrameNumbers('player', { start: 5, end: 8 }),
         frameRate: 10,
         repeat: -1
     });
